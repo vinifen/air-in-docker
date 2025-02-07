5e46f5c7bfeeb48523c8239c664ca64dabf1786a
vinifen <ferreiranivicius@gmail.com>
2025-02-07 18:24:01 -0300

    ✨ Nginx worked in browser

--- web/setVariables.sh
+++ web/setVariables.sh
@@ -5,8 +5,11 @@ echo "WEB_DOMAIN: ${WEB_DOMAIN}"
 echo "FRONT_URL: ${FRONT_URL}"
 echo "BACK_URL: ${BACK_URL}"
 
-envsubst '${WEB_PORT} ${WEB_DOMAIN} ${FRONT_URL} ${BACK_URL}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
+# Usando envsubst para substituir variáveis de ambiente no nginx.conf
+envsubst '${WEB_PORT} ${WEB_DOMAIN} ${FRONT_URL} ${BACK_URL} ${WEB_FRONT_PATH} ${WEB_BACK_PATH}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+# Exibindo o conteúdo do nginx.conf após a substituição
 cat /etc/nginx/nginx.conf
 
+# Iniciando o Nginx
 exec nginx -g 'daemon off;'

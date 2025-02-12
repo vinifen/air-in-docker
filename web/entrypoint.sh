0d0490039786baf40e0fc3e26248ea33a96716d3
vinifen <ferreiranivicius@gmail.com>
2025-02-12 10:12:31 -0300

    📦 Nginx and docker updated and search-bar sm bug fixed

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -7,7 +7,7 @@ echo "WEB_DOMAIN: ${WEB_DOMAIN}"
 echo "FRONT_URL: ${FRONT_URL}"
 echo "BACK_URL: ${BACK_URL}"
 
-envsubst '${WEB_PORT} ${WEB_DOMAIN} ${FRONT_URL} ${BACK_URL} ${WEB_FRONT_PATH} ${WEB_BACK_PATH}' < /web/nginx.conf > /etc/nginx/nginx.conf
+envsubst '${WEB_PORT} ${WEB_DOMAIN} ${FRONT_URL} ${BACK_URL} ${WEB_FRONT_PATH} ${WEB_BACK_PATH} ${WEB_AUTH_PATH}' < /web/nginx.conf > /etc/nginx/nginx.conf
 
 cat /etc/nginx/nginx.conf
 
